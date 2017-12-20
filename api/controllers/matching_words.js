79f66c42619d9319a4a9bb0bec945d909607c35d
Gordon Krefting <gordon@krefting.org>
2017-12-20 09:07:48 -0500

    got more tests working

--- api/controllers/matching_words.js
+++ api/controllers/matching_words.js
@@ -1,22 +1,45 @@
 'use strict';
 
+const util = require('util'); //?
+const { exec } = require('child_process');
+
 module.exports = {
   matching_words: matching_words
 };
+
 function matching_words(request, response) {
-  var q = request.swagger.params.q.value;
+  var q = request.swagger.params.q.value.toLowerCase();
 
-  var pattern = /[^a-z\.]+/;
-
+  // '' -> []
   if (q === '') {
     response.json([]);
     return;
   }
 
+  // only letters and '.' allowed
+  var pattern = /[^a-z\.]+/;
   if (q.match(pattern)) {
     response.json([]);
     return;
 	}
 
-  response.json(["These","are","some","matching", "words", "tree"]);
+	var cmd = `/usr/bin/grep "^${q}$" /usr/share/dict/words | head -n 101`;
+	
+	exec(cmd, (err, stdout, stderr) => {
+
+	  if (err) {
+	  	console.log(`error: ${err}`);
+	  	// How to throw errors?
+	    // node couldn't execute the command
+	    return;
+	  }
+
+	  // the *entire* stdout and stderr (buffered)
+	  if (stdout === '') {
+	  	response.json([]);
+	  } else {
+		  response.json(stdout.split("\n"));
+	  }
+	  return;
+	});
 }
